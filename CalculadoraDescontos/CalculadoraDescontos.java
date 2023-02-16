73a267fe9ee9e795ad0d1f0e966fcdeb19975de0
goisygor <ygorgoissenai@gmail.com>
2023-02-16 20:53:46 -0300

    16/02/23

--- CalculadoraDescontos/CalculadoraDescontos.java
+++ CalculadoraDescontos/CalculadoraDescontos.java
@@ -1,5 +1,34 @@
 package CalculadoraDescontos;
 
+import java.util.Scanner;
+
 public class CalculadoraDescontos {
+    public void calcular20(){
+        Scanner sc = new Scanner(System.in); 
+        System.out.println("informe o Preço do Produto");
+        double precoProduto = sc.nextDouble();
+        final int desconto20 = 20;
+        double precoComDesconto= precoProduto- 
+        (precoProduto*desconto20/100);
+        System.out.println ("O Preço com desconto é"+precoComDesconto+"reais");
+        sc.close();
+    }
+
+
+
+    public void calcular30(){
+        double precoProduto;
+        precoProduto=450;
+        final int desconto30 = 30;
+        double precoComDesconto= precoProduto- 
+        (precoProduto*desconto30/100);
+        System.out.println ("O Preço com desconto é"+precoComDesconto+"reais");
     
-}
+
+    }
+    }
+
+    
+
+
+
---
+++ Operacoes/App.java
@@ -0,0 +1,10 @@
+package Operacoes;
+
+public class App {public static void main(String[] args) {
+    Operadores objeto1 = new Operadores();
+    objeto1.aritimetica();
+    objeto1.atribuicao();
+    objeto1.relacionais();
+}
+    
+}
